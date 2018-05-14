6deb26baf6cff3c98467c2e0d5e080ac851f02de
migingram <miguelingrammedina@hotmail.com>
2018-05-14 19:40:21 +0200

    4th Commit - Paddle and Movement range

--- BrickBreaker.java
+++ BrickBreaker.java
@@ -9,12 +9,13 @@ public class BrickBreaker
         Rectangle pointsBar = new Rectangle(865,440,170,280,"#020202");
         template.addRectangle(rightColumn);
         template.addRectangle(pointsBar);
-        template.update();
-        
-        Arrow aim = new Arrow(380,580,380,550,2,"#FFFFFF",template);
-        
+            
+    	    
+    	Arrow aim = new Arrow(380,580,380,550,2,"#FFFFFF",template);
+
+    	    
         Rectangle[] b = new Rectangle[25];
-	    
+    	    
         int marginX = 0;
         int marginY = 0;
       
@@ -49,18 +50,34 @@ public class BrickBreaker
           
             marginX = marginX + 90;
             template.update();
+            
         }
         
-        while (true)
+        
+        
+        boolean change = false;
+        
+        Rectangle paddle1 = new Rectangle(385, 580, 80, 10, "FFFFFF");
+	    Rectangle paddle2 = new Rectangle(352, 585, 14, 20, "FFFFFF");
+	    Rectangle paddle3 = new Rectangle(418, 585, 14, 20, "FFFFFF");
+	            
+        
+	    while (true)
         {   
-             while ((aim.getStartX() <= aim.getEndX()))
+             while ((aim.getStartX() <= aim.getEndX()) && (change == false))
              {
             	 
             	 if (template.spacePressed())
                  {
             		 aim.setColour("000000");
-
+         	         
+         	         template.addRectangle(paddle1);
+                     template.addRectangle(paddle2);
+                     template.addRectangle(paddle3);
+                    
                      template.update();
+          	         
+          	         change = true;
                  }
             	 
             	 
@@ -79,7 +96,7 @@ public class BrickBreaker
              }
              
 
-             while ((aim.getStartX() > aim.getEndX()))
+             while ((aim.getStartX() > aim.getEndX()) && (change == false))
              {     
             	 
             	 
@@ -87,8 +104,13 @@ public class BrickBreaker
                  {
          	         aim.setColour("000000");
          	         
+         	         template.addRectangle(paddle1);
+                     template.addRectangle(paddle2);
+                     template.addRectangle(paddle3);
+                    
                      template.update();
-          	              
+          	         
+          	         change = true;       
                  }
             	      	 
             	 
@@ -106,7 +128,31 @@ public class BrickBreaker
                    template.update();
              }
              
-        }
-
+             
+             while (change == true)
+             {
+            	 if ((template.rightPressed()) && (paddle1.getXPosition() < 700))
+     	         {
+     		         paddle1.setXPosition(paddle1.getXPosition() + 10);
+     		         paddle2.setXPosition(paddle2.getXPosition() + 10);
+     		         paddle3.setXPosition(paddle3.getXPosition() + 10);
+     	         }
+                
+                if ((template.leftPressed()) && (paddle1.getXPosition() > 55))
+     	         {
+     		         paddle1.setXPosition(paddle1.getXPosition() - 10);
+     		         paddle2.setXPosition(paddle2.getXPosition() - 10);
+     		         paddle3.setXPosition(paddle3.getXPosition() - 10);
+     	         }
+                 
+                 
+                template.pause();
+                template.update();
+             }       
+        }  
     }
 }
+
+
+                                                              
+
