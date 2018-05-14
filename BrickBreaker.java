41ae896190093548a96e62bcacd8567669b0677d
migingram <miguelingrammedina@hotmail.com>
2018-05-14 19:30:10 +0200

    1st Commit - Implementing UI

---
+++ BrickBreaker.java
@@ -0,0 +1,17 @@
+public class BrickBreaker
+{         
+    public static void main(String[] args)
+    {   
+    	
+    	GameArena template = new GameArena(1000,600); // Creates window of specified dimensions
+    	
+    	Rectangle rightColumn = new Rectangle(880,500,250,1000,"#202C4F"); 
+        Rectangle pointsBar = new Rectangle(865,440,170,280,"#020202");
+        template.addRectangle(rightColumn);
+        template.addRectangle(pointsBar);
+        template.update();
+    }
+}
+
+
+                                                              
